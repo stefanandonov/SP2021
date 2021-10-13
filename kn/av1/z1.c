80aad5d0d87a3179b554a36e7c74a736927cf4de
stefanandonov <stefan5andonov@yahoo.com>
2021-10-13 15:50:40 +0200

    Initial commit

---
+++ kn/av1/z1.c
@@ -0,0 +1,13 @@
+//
+// Created by Stefan Andonov on 10/13/21.
+//
+#include<stdio.h>
+int main (){
+    int x = 5;
+    float y = 6.676767;
+    printf("%d %.2f\n", x, y);
+    printf("Dobredojdovte na kursot SP\n");
+    printf("Milo mi e. Dobro ve najdov!");
+    return 0;
+}
+
